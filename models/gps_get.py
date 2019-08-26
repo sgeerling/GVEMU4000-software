acfc6ceeb362a549e94fcd6b15e0312b8980e64f
gopimn <gopimn@gmail.com>
2019-08-26 14:16:02 -0400

    testing assembly of gtfri frame

--- models/gps_get.py
+++ models/gps_get.py
@@ -8,13 +8,19 @@ class GpsPoller(threading.Thread):
   def __init__(self):
     print("00\n")
     threading.Thread.__init__(self)
-    global share.gpsd #bring it in scope
+    print("01\n")
+    #global share.gpsd #bring it in scope
     share.gpsd = gps(mode=WATCH_ENABLE) #starting the stream of info
+    print("02\n")
     self.current_value = None
+    print("03\n")
     self.running = True #setting the thread running to true
-
+    print("04\n")
+    
   def run(self):
     #global gpsd
+    print("06\n")
     while self.running:
-      share.gpsd.next() #this will continue to loop and grab EACH set of gpsd info to clear the buffer
+        print("07\n")
+        share.gpsd.next() #this will continue to loop and grab EACH set of gpsd info to clear the buffer
 
