40704eb65934d3a14bbff8d43967cbe83b672f7f
gopimn <gopimn@gmail.com>
2019-08-26 06:34:40 -0400

    sepaerd gps frm device class

--- gvemu4000.py
+++ gvemu4000.py
@@ -2,20 +2,50 @@
 """
 @author: gopimn
 """
-
+import os
+from gps import *
+from time import *
+import time
+import threading
 from models.device import device as dev
-
+gpsd = None #seting the global variable
 # these parameters are globals for now.
 # when one of those is None, theres  no excecution of the timer thread,
-# check if this is spending CPU
 params = {}
 params['period_gtfri'] = 0.4
 params['period_gtinf'] = 0.3
+
+class GpsPoller(threading.Thread):
+  def __init__(self):
+    threading.Thread.__init__(self)
+    global gpsd #bring it in scope
+    gpsd = gps(mode=WATCH_ENABLE) #starting the stream of info
+    self.current_value = None
+    self.running = True #setting the thread running to true
+
+  def run(self):
+    global gpsd
+    while gpsp.running:
+      gpsd.next() #this will continue to loop and grab EACH set of gpsd info to clear the buffer
+
 
 def main():
   gvemu = dev(params)
   gvemu.start()
   print("threads started\n") 
+  gpsp = GpsPoller() # create the thread
+  try:
+    while True:
+      gpsp.start() # start it up
+      print ("latitude  " +gpsd.fix.latitude)
+      time.sleep(5)
+      
+  except (KeyboardInterrupt, SystemExit): #when you press ctrl+c
+    print "\nKilling Thread..."
+    gpsp.running = False
+    gpsp.join() # wait for the thread to finish what it's doing
+    
+  print "Done.\nExiting."
   
 if __name__== "__main__":
   main()
--- models/device.py
+++ models/device.py
@@ -2,39 +2,16 @@
 
 from utils.utils import perpetualTimer as timer
 from time import ctime
-from gps import *
-import time
-import threading
-
-gpsd = None #seting the global variable
-
-class GpsPoller(threading.Thread):
-  def __init__(self):
-    threading.Thread.__init__(self)
-    global gpsd #bring it in scope
-    gpsd = gps(mode=WATCH_ENABLE) #starting the stream of info
-    self.current_value = None
-    self.running = True #setting the thread running to true
-
-  def run(self):
-    global gpsd
-    while gpsp.running:
-      gpsd.next() #this will continue to loop and grab EACH set of gpsd info to clear the buffer
 
 class device(object):
     
     def gtfri_method(self,test_var = None): 
         print("gtfri into queue\n")
-        print(gpsd.utc)
         
     def gtinf_method(self,test_var = None): 
         print("gtinf into queue\n")
-        print(gpsd.fix.latitude)
         
     def __init__(self,params):
-        #global gpsd #bring it in the device class scope
-        gpsp = GpsPoller() # create the thread
-        gpsp.start() # start it up try here
         self.params = params
         self.born_date = ctime()
         if 'period_gtfri' in params.keys():
@@ -49,4 +26,3 @@ class device(object):
             self.timer_gtinf.start()
 
 
-#https://stackoverflow.com/questions/3295065/gpsd-python-client
