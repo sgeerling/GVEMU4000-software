e9572d2692b0a5e2a55850c5161f28f4778826ce
gopimn <gopimn@gmail.com>
2019-08-26 13:47:19 -0400

    testing assembly of gtfri frame

--- models/gps_get.py
+++ models/gps_get.py
@@ -13,6 +13,6 @@ class GpsPoller(threading.Thread):
 
   def run(self):
     global gpsd
-    while self.running:
+    while gpsd.running:
       gpsd.next() #this will continue to loop and grab EACH set of gpsd info to clear the buffer
 
