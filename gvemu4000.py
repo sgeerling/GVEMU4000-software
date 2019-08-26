32a9706c81be5d8d1bd043957c1b626a3b01b0a7
gopimn <gopimn@gmail.com>
2019-08-26 08:51:58 -0400

    testing imei reading

--- gvemu4000.py
+++ gvemu4000.py
@@ -38,7 +38,7 @@ def main():
     gpsp.start() # start it up
     while True:
       print ("latitude  " +str(gpsd.fix.latitude))
-      raw =os.popen("cat /var/log/messages | grep 'AT+GSN' -A 1 | tail -1")
+      raw =os.system("cat /var/log/messages | grep 'AT+GSN' -A 1 | tail -1")
       print(raw)
       time.sleep(5)
       
