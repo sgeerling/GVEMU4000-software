375080eb94f5358df6392e1929a2fec63e8eff38
gopimn <gopimn@gmail.com>
2019-08-30 08:43:56 -0400

    fixed typo

--- models/device.py
+++ models/device.py
@@ -113,7 +113,7 @@ class device(object):
         aux=aux.split(',')
 
         to_kam=aux[3]+aux[4]+aux[5]
-        self.serialport.write(to_kam.decode())
+        self.serialport.write(bytes(to_kam))
         # - [ ] Check if the port is open
         # - [ ] Check if the port has available data before calling readline
 
