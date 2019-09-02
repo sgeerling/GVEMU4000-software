f1594a9017d44053df4c36a29dd1ee8af371f188
gopimn <gopimn@gmail.com>
2019-09-02 18:47:15 -0400

    added GTDAT parsing

--- utils/utils.py
+++ utils/utils.py
@@ -19,7 +19,7 @@ def is_gtdat(data):
     aux = str(data).split(",")
     if (len(aux) >10):
         print(aux[0])
-        if (str(aux[0]) == "AT+GTDAT=gv300w"):
+        if (str(aux[0]) == b'AT+GTDAT=gv300w'):
             return True
     return False
             
