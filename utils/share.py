8859647fe0cb85e6ea85faea2712297049cc9e84
gopimn <gopimn@gmail.com>
2019-09-04 17:23:36 -0400

    Implementing logging lib #17

--- utils/share.py
+++ utils/share.py
@@ -12,8 +12,6 @@ logger = logging.getLogger(__name__)
 c_handler = logging.StreamHandler() # Log for display
 f_handler = logging.FileHandler('gvemu_test.log', mode='a') # Log for file
 
-logger.addHandler(c_handler)
-logger.addHandler(f_handler)
 
 formattc = logging.Formatter('[%(asctime)s](%(levelname)s) eBot: %(message)s', datefmt='%d%m%y-%H:%M:%S')
 formattf = logging.Formatter('[%(asctime)s](%(levelname)s) eBot: %(message)s', datefmt='%d%m%y-%H:%M:%S')
@@ -23,5 +21,7 @@ f_handler.setFormatter(formattf)
 
 c_handler.setFormatter(formattc)
 #logger.setLevel(logging.DEBUG)
+logger.addHandler(c_handler)
+logger.addHandler(f_handler)
 
 logger.info("logger started")
