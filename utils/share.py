d6b4b594f2227429a0d712e3ce3e0754b52a8e5d
gopimn <gopimn@gmail.com>
2019-09-04 17:30:10 -0400

    LOGS READY #17

--- utils/share.py
+++ utils/share.py
@@ -13,8 +13,8 @@ c_handler = logging.StreamHandler() # Log for display
 f_handler = logging.FileHandler('test.log', mode='a') # Log for file
 
 
-formattc = logging.Formatter('[%(asctime)s](%(levelname)s) eBot: %(message)s', datefmt='%d%m%y-%H:%M:%S')
-formattf = logging.Formatter('[%(asctime)s](%(levelname)s) eBot: %(message)s', datefmt='%d%m%y-%H:%M:%S')
+formattc = logging.Formatter('[%(asctime)s](%(levelname)s %(name)s) eBot: %(message)s', datefmt='%d%m%y-%H:%M:%S')
+formattf = logging.Formatter('[%(asctime)s](%(levelname)s %(name)s) eBot: %(message)s', datefmt='%d%m%y-%H:%M:%S')
 
 c_handler.setFormatter(formattc)
 f_handler.setFormatter(formattf)
