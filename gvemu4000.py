06cd75bce1fe78ccb6ad486617ef5d327015c637
gopimn <gopimn@gmail.com>
2019-09-04 16:47:28 -0400

    Implementing logging lib #17

--- gvemu4000.py
+++ gvemu4000.py
@@ -35,10 +35,10 @@ def main():
         gpsp.start()
         gvemu = dev(params)
         gvemu.start()
-        logger.info("threads started!")
+        share.logger.info("threads started!")
         while True:
             if share.to_server:
-                logger.debug("server queue not empty!")
+                share.logger.debug("server queue not empty!")
                 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
                 # UGLY HARDCODE
                 s.connect((server_ip_add, server_port))
@@ -48,26 +48,26 @@ def main():
                     str_to_server +=\
                                   str((datetime.now().strftime("%Y%m%d%H%M%S")))
                     str_to_server += ",FFFF$"
-                    logger.info("Transmitting: %s", str_to_server)
+                    share.logger.info("Transmitting: %s", str_to_server)
                     try:
                         s.sendall(str_to_server.encode())
                         time.sleep(0.1)
                         data = s.recv(1024)
                         if data:
-                            logger.info("recieved from server: %s", str(data))
+                            share.logger.info("recieved from server: %s", str(data))
                             gvemu.send_to_kam(data)
                     except socket.timeout as e:
-                        logger.error("Exception raised:", exc_info=True)
+                        share.logger.error("Exception raised:", exc_info=True)
                     finally:
                         s.close()
-                    logger.info("I'm alive")
+                    share.logger.info("I'm alive")
                     time.sleep(1)
 
     except (KeyboardInterrupt, SystemExit): #when you press ctrl+c
-        logger.error("Killing thread")
+        share.logger.error("Killing thread")
         share.gpsp.running = False
         share.gpsp.join() # wait for the thread to finish what it's doing
-        logger.error("ciao =)")
+        share.logger.error("ciao =)")
         
 
 if __name__== "__main__":
