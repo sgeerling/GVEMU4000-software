5c6fd0452ef78a0f53fd3a5f12a588ff42a81509
gopimn <gopimn@gmail.com>
2019-09-04 15:37:45 -0400

    Implementing logging lib #17

--- gvemu4000.py
+++ gvemu4000.py
@@ -29,16 +29,14 @@ logger = logging.getLogger(__name__)
 c_handler = logging.StreamHandler() # Log for display
 f_handler = logging.FileHandler('gvemu_test.log', mode='a') # Log for file
 
-
+c_handler.basicConfig(level=logging.DEBUG)
+f_handler.basicConfig(level=logging.DEBUG)
 
 c_format = logging.Formatter('%(asctime)s-eBot %(levelname)s: %(message)s')#, datefmt='%d-%m-%y %H:%M:%S')
 f_format = logging.Formatter('%(asctime)s-eBot %(levelname)s: %(message)s')#, datefmt='%d-%m-%y %H:%M:%S')
 
 c_handler.setFormatter(c_format)
 f_handler.setFormatter(f_format)
-
-c_handler.setLevel(logging.DEBUG)
-f_handler.setLevel(logging.DEBUG)
 
 logger.addHandler(c_handler)
 logger.addHandler(f_handler)
