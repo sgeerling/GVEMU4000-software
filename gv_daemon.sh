5d0ca02e39fc55ebaf4c259e794660f8921a1bb5
gopimn <gopimn@gmail.com>
2019-09-04 18:42:08 -0400

    itaeration on daemon

--- gv_daemon.sh
+++ gv_daemon.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-/home/debian/git/GVEMU4000-software/virtual_python/bin/python3 /home/debian/git/GVEMU4000-software/gvemu4000.py
+source "/home/debian/git/GVEMU4000-software/virtual_python/bin/activate"
+python3 /home/debian/git/GVEMU4000-software/gvemu4000.py
