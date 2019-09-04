90ca3f54e928e8ade11f6fc5d3101f4140e9a3d3
gopimn <gopimn@gmail.com>
2019-09-04 15:14:19 -0400

    Implementing logging lib #17

--- gvemu4000.py
+++ gvemu4000.py
@@ -49,10 +49,10 @@ def main():
         gpsp.start()
         gvemu = dev(params)
         gvemu.start()
-        logging.info("threads started!")
+        logger.info("threads started!")
         while True:
             if share.to_server:
-                logging.info("server queue not empty!")
+                logger.info("server queue not empty!")
                 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
                 # UGLY HARDCODE
                 s.connect((server_ip_add, server_port))
@@ -62,26 +62,26 @@ def main():
                     str_to_server +=\
                                   str((datetime.now().strftime("%Y%m%d%H%M%S")))
                     str_to_server += ",FFFF$"
-                    logging.info("Transmitting: %s", str_to_server)
+                    logger.info("Transmitting: %s", str_to_server)
                     try:
                         s.sendall(str_to_server.encode())
                         time.sleep(0.1)
                         data = s.recv(1024)
                         if data:
-                            logging.info("recieved from server: %s", str(data))
+                            logger.info("recieved from server: %s", str(data))
                             gvemu.send_to_kam(data)
                     except socket.timeout as e:
-                        logging.error("Exception raised:", exc_info=True)
+                        logger.error("Exception raised:", exc_info=True)
                     finally:
                         s.close()
-                    logging.info("I'm alive")
+                    logger.info("I'm alive")
                     time.sleep(1)
 
     except (KeyboardInterrupt, SystemExit): #when you press ctrl+c
-        logging.error("Killing thread")
+        logger.error("Killing thread")
         share.gpsp.running = False
         share.gpsp.join() # wait for the thread to finish what it's doing
-        logging.error("ciao =)")
+        logger.error("ciao =)")
         
 
 if __name__== "__main__":
