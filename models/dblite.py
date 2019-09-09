7bf5d2a6a431a2e2d34f99cf3d1e710748ad2204
gopimn <gopimn@gmail.com>
2019-09-09 09:34:31 -0300

    function get_last_insert_rowid implemented #17

--- models/dblite.py
+++ models/dblite.py
@@ -94,7 +94,7 @@ class MyDatabase:
         with self.db_engine.connect() as connection:
             try:
                 res = connection.execute(query)
-                print(res)
+                print(int(res))
             except Exception as e:
                 print(e)
         #a = self.execute_query(query)
